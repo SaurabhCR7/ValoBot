60ef74252827bc5b639862d461587b12e4c0ef32
Saurabh Chauhan <saurabh.chauhan@nagarro.com>
2021-08-14 16:10:51 +0530

    Added new version code

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
-const commands = [
-	{
-		name: 'ping',
-		description: 'Replies with Pong!',
-	},
-];
-
-const rest = new REST({ version: '9' }).setToken('token');
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-			body: commands,
-		});
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('interactionCreate', async (interaction) => {
-	if (!interaction.isCommand()) return;
-
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
+client.on('messageCreate', (message) => {
+	if (message.content === 'ping') {
+		message.channel.send('pong');
 	}
 });
 
