471521cbaad3d7e79296046516bbc7be6e7116e2
Saurabh Chauhan <saurabh.chauhan@nagarro.com>
2021-08-15 11:09:29 +0530

    Added quote functionality

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const getQuote = () => {
+	fetch('https://zenquotes.io/api/random')
+		.then((res) => res.json())
+		.then((data) => data[0]['q'] + ' - ' + data[0]['a']);
+};
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
-	if (message.content === 'ping') {
-		message.channel.send('pong');
+client.on('messageCreate', (msg) => {
+	if (msg.author.bot) return;
+
+	if (msg.content === 'inspire') {
+		getQuote.then((quote) => msg.channel.send(quote));
+	}
+
+	if (msg.content === 'ping') {
+		msg.channel.send('pong');
 	}
 });
 
